01adb68c0d558d379c4c45ef76baeed7aaf2f00f
Lankesh <lankeshpathmaraj@gmail.com>
2026-05-11 11:52:45 +0530

    feat: add user model

---
+++ src/main/java/com/smartsec/smartsec_api/model/User.java
@@ -0,0 +1,45 @@
+package com.smartsec.smartsec_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "user", cascade =CascadeType.ALL, fetch = FetchType.LAZY )
+    private List<Scan> scans;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
